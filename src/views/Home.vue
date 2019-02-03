f9325d8f30a2553760324946332ef5773a172a6f
Andrei0872 <andreigtj01@gmail.com>
2019-02-03 11:16:03 +0200

    basics of transitions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BasicTransitions msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
+import BasicTransitions from '../components/Basic Tranisitons/Main';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    BasicTransitions,
   },
 };
 </script>
