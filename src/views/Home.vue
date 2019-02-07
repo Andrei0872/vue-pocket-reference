a5f15f0cf7443a6ce194d500841ad494de301ee0
Andrei0872 <andreigtj01@gmail.com>
2019-02-08 00:25:23 +0200

    abstract component with IntersectionObserver

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div style="margin: 100vh auto; border: 1px solid green;">
+      <AbstractComp @intersect-enter="handleEnter" @intersect-leave="handleLeave">
+      <h3>My parent comp is an abstract component!!!</h3>
+    </AbstractComp>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
+import AbstractComp from '@/components/AbstractComp.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    AbstractComp,
+  },
+  methods: {
+    handleEnter () {
+      console.log('entering...')
+    },
+    handleLeave () {
+      console.log('leaving...')
+    }
   },
 };
 </script>
