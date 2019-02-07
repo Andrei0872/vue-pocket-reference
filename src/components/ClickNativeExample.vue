95b7883b8d19d9976884b36fd061ab87d53857b6
Andrei0872 <andreigtj01@gmail.com>
2019-02-07 18:58:58 +0200

    .native modifier

---
+++ src/components/ClickNativeExample.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <!-- Works without the `.native` modifer on this component -->
+        <!-- <h3 @click="$emit('click')"><i>.native</i> modifier</h3> -->
+        <h3><i>.native</i> modifier</h3>
+    </div>
+</template>
+
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="hello">
     <h1>Time to build awesome things!</h1>
+    <!-- This requires emitting an event from the child component -->
+    <!-- <ClickNativeExample @click="clickFn" /> -->
+
+    <!-- This would simply work as the event is attached to the root component -->
+    <ClickNativeExample @click.native="clickFn" />
   </div>
 </template>
 
 <script>
+import ClickNativeExample from './ClickNativeExample';
+
 export default {
   name: 'HelloWorld',
+  components: {
+    ClickNativeExample
+  },
   props: {
     msg: String,
   },
@@ -15,6 +25,11 @@ export default {
     console.log(this.$el); // <div class="hello"></div>
     // The root vue instance of the current component tree
     console.log(this.$root); // <div id="app"></div>
+  },
+  methods: {
+    clickFn () {
+      alert('clicked!');
+    }
   }
 };
 </script>
