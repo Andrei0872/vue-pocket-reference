33c7fa55cb429ead11083f9255b00b8826ef4db6
Andrei0872 <andreigtj01@gmail.com>
2019-02-09 00:23:09 +0200

    understanding the new syntax of slots

---
+++ src/NewFeatures.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <slot name="name"></slot>
+        <!-- Passing data to the parent component -->
+        <slot name="age" :passion="passion"></slot>
+        <slot></slot>
+        <slot name="testSlot"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    passion: 'programming',
+  }),
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,40 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NewFeatures>
+
+      <template v-slot:[customSlot]>
+        <h3>{{ name }}</h3>
+      </template>
+
+      <!-- Using the data passed from the child component -->
+      <template v-slot:age="{ passion }">
+        <h3>{{ age }}</h3>
+        <h2>My passions is {{ passion }}</h2>
+      </template>
+
+      <template>
+        <p>This is just a default slot</p>
+      </template>
+
+    </NewFeatures>
+
+    <button @click="customSlot = customSlot === 'name' ? 'testSlot' : 'name'">Change Slot Name!</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
+import NewFeatures from '../NewFeatures.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    NewFeatures,
   },
+  data: () => ({
+    name: 'Andrei',
+    age: 17,
+    customSlot: 'name',
+  }),
 };
 </script>
