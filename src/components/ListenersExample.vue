d1ce25ee82e32bcd94d66d6d803193abd11d9f98
Andrei0872 <andreigtj01@gmail.com>
2019-02-10 21:25:24 +0200

    :sparkles: $listeners example

---
+++ src/components/ListenersExample.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <!-- Assign the events in the right place -->
+        <input type="text" v-on="$listeners">
+    </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld />
+    <br>
+    <ListenersExample @focus="showFocus"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from '@/components/HelloWorld.vue';
+import ListenersExample from '@/components/ListenersExample';
 
 export default {
   name: 'home',
   components: {
     HelloWorld,
+    ListenersExample,
+  },
+  methods: {
+    showFocus() {
+      console.log('input focused!');
+    },
   },
 };
 </script>
