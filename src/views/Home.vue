0a0527ffa5ad4b0fde03db920e6a922f889b480d
Andrei0872 <andreigtj01@gmail.com>
2019-02-10 22:33:40 +0200

    $attrs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,24 +3,39 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld />
     <br>
+    <h3><mark>$listeners</mark> example (check the console)</h3>
     <ListenersExample @focus="showFocus"/>
+    <br>
+    <h3><mark>inheritAttrs</mark> example</h3>
+    <InheritAttrs rows="50" :divStyle="square"/>  
   </div>
 </template>
 
 <script>
 import HelloWorld from '@/components/HelloWorld.vue';
 import ListenersExample from '@/components/ListenersExample';
+import InheritAttrs from '@/components/InheritAttrs';
 
 export default {
   name: 'home',
   components: {
     HelloWorld,
     ListenersExample,
+    InheritAttrs,
   },
+  data: () => ({
+    square: {
+        border: '1px solid black',
+        width: "30px",
+        height: "30px",
+    }
+  }),
   methods: {
     showFocus() {
       console.log('input focused!');
     },
   },
+  computed: {
+  }
 };
 </script>
