813d08de131a4fe90c1b10c81ff213be961ce623
andrei <andreigtj01@gmail.com>
2019-02-14 12:31:38 +0200

    customizable tabs v1

---
+++ src/TabList.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+
+        <div class="tab_container">
+            <ul class="tab_container__list">
+                <li
+                    v-for="tabName in tabNames"
+                    :key="tabName"
+                    :class="{ 
+                        tab_container__item: true,
+                        'tab_container__item--active': currentTab === tabName
+                    }"
+                    @click="currentTab = tabName"
+                >
+                    {{ tabName }}
+                </li>
+            </ul>
+            
+            <div class="tab_container__content">
+                <p
+                    v-for="tabName in tabNames"
+                    :key="tabName"
+                >
+                    <slot 
+                        :name="tabName"
+                        v-if="currentTab === tabName"
+                        :tabUpper="`This is the ${tabName} tab`"
+                    ></slot>
+                </p>
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        currentTab: String
+    },
+    data: () => ({
+        tabNames: ['home', 'about', 'contact']
+    }),
+}
+</script>
+
+
+<style scoped>
+
+.tab_container {
+    width: 25rem;
+    height: 25rem;
+    margin: 50px auto;
+    border: 1px solid #ccc;
+}
+
+.tab_container__list {
+    list-style: none;
+    display: flex;
+}
+
+.tab_container__item {
+    flex: 1 1 auto;
+}
+
+.tab_container__item:not(:last-child) {
+    border-right: 1px solid #ccc;
+}
+
+.tab_container__item {
+    cursor: pointer;
+}
+
+.tab_container__item--active {
+    color: green;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <NewFeatures>
+    <!-- Modify the value of `newFeatures` in order to visualize this example -->
+    <NewFeatures v-if="newFeatures">
 
       <template v-slot:[customSlot]>
         <h3>{{ name }}</h3>
@@ -19,22 +20,52 @@
 
     </NewFeatures>
 
-    <button @click="customSlot = customSlot === 'name' ? 'testSlot' : 'name'">Change Slot Name!</button>
+    <button v-if="newFeatures" @click="customSlot = customSlot === 'name' ? 'testSlot' : 'name'">Change Slot Name!</button>
+
+    <!-- Modify the value of `tabList` in order to visualize this example -->
+    <!-- The content should remain in the template -->
+    <!-- The script should contain the logic -->
+    <TabList v-if="tabList" :currentTab="selectedTab">
+
+      <template v-slot:home="{ tabUpper }">
+        <h3>Home</h3>
+        <h3>{{ tabUpper }}</h3>
+        <p>Home content</p>
+      </template>
+
+      <template v-slot:about="{ tabUpper }">
+        <h3>About</h3>
+        <h3>{{ tabUpper }}</h3>
+        <p>About content</p>
+      </template>
+
+      <template v-slot:contact="{ tabUpper }">
+        <h3>contact</h3>
+        <h3>{{ tabUpper }}</h3>
+        <p>contact content</p>
+      </template>
+
+    </TabList>
   </div>
 </template>
 
 <script>
 import NewFeatures from '../NewFeatures.vue';
+import TabList from '../TabList.vue';
 
 export default {
   name: 'home',
   components: {
     NewFeatures,
+    TabList,
   },
   data: () => ({
     name: 'Andrei',
     age: 17,
     customSlot: 'name',
+    newFeatures: false,
+    tabList: true,
+    selectedTab: 'home',
   }),
 };
 </script>
